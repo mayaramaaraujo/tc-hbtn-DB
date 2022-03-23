4f8e51391f030442f322093665938dba914c3ca4
mayaramaaraujo <mayaramaaraujo98@gmail.com>
2022-03-23 09:04:29 -0300

    saving

---
+++ 0x00/salvando/sql_schema_first_db.sql
@@ -0,0 +1,15 @@
+-- TABLE
+CREATE TABLE CLIENTE(
+id integer not null,
+nome varchar(80) not null,
+email varchar (40) not null,
+telefone int,
+PRIMARY KEY(Id)
+);
+ 
+-- INDEX
+ 
+-- TRIGGER
+ 
+-- VIEW
+ 
